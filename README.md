4cb6c9712ce09c45a7f28b30f6860479f39bcb16
Ethan Ramos <ethanramos@Ethans-MacBook-Pro-7.local>
2024-02-05 18:20:22 -0500

    Added function headers to the game

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # dice-rolling
-# dice-rolling
---
+++ diceRoller.py
@@ -0,0 +1,19 @@
+import questionary
+import random
+from termcolor import colored
+
+
+def getNumDice():
+    pass
+
+
+def getNumSides():
+    pass
+
+
+def printRolls(rolls):
+    pass
+
+
+def diceSimulator():
+    pass
